07d1aa9a540a7a04409ebf7b2a107fd5d964c2da
Asistente Infraestructura Informatica <sistemas2@Oreplast.local>
2023-11-21 15:35:41 -0600

    quito commit

--- wwwroot/js/Paginacion.js
+++ wwwroot/js/Paginacion.js
@@ -1,3 +1,16 @@
 ﻿$(document).ready(function () {
-    $('#table_id').DataTable();
+    $('#table_id').DataTable({
+        language: {
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sEmptytable": "Ningun dato disponible en esta tabla ",
+            "sInfo": "Mostrando registros del _START_ al _END_ de una tabla de _TOTAL_",
+            "sSearch": "Buscar: ",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Ultimo",
+                "sNext": "Siguiente",
+                "sPrevious":"Anterior"
+            }
+        }
+    });
 });
